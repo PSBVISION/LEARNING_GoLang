623574806cfe372e0cb07e4e5420ccb59b4ad781
psbvision <punitsinghbisht1529@gmail.com>
2025-05-12 20:56:22 +0530

    learned about simple values

---
+++ 2_simple_values/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	//simple values
+	//int
+	fmt.Println(4)
+	//float
+	fmt.Println(4.5)
+	fmt.Println(4.5/3.0)
+	//string
+	fmt.Println("Hello World")
+	//bool
+	fmt.Println(true)
+	fmt.Println(false)
+}
