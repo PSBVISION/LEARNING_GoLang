bf944400628ad589155417734ebc0af854a9a1ef
psbvision <punitsinghbisht1529@gmail.com>
2025-09-14 19:32:29 +0530

    learn about variables

---
+++ 3_variables/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "hmmm"
+
+	// infer
+	var lastName = "hmmmmmmmmmmm"
+	var isReal = true
+
+	//shorthand syntax
+	notGonnaTell := "hmmmmmmmmmmmmmmmmmmmm"
+
+	//assign later
+	var age int
+	age=20
+
+	fmt.Println(name)
+	fmt.Println(lastName)
+	fmt.Println(isReal)
+	fmt.Println(notGonnaTell)
+	fmt.Println(age)
+}
